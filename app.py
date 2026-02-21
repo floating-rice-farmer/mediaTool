f5ad29ec952e67ba54d61ff35b00a2347052f9f5
Skye Liu <magicalelectropants@gmail.com>
2026-02-21 12:22:09 +1100

    added frontedn

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, send_file
+import yt_dlp
+import os
+import tempfile
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/download', methods=['POST'])
+def download():
+    url = request.form.get('url')
+    tmp_dir = tempfile.mkdtemp()
+    
+    ydl_opts = {
+        'format': 'best',
+        'outtmpl': os.path.join(tmp_dir, '%(title)s.%(ext)s'),
+        'noplaylist': True,
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            filename = ydl.prepare_filename(info)
+            return send_file(filename, as_attachment=True)
+    except Exception as e:
+        return f"Error: {str(e)}", 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
